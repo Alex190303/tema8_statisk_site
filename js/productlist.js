a038c63379804521a04a0083b6c9190820f05ae2
Alex190303 <corneliahjort@gmail.com>
2026-03-02 15:18:48 +0100

    index-dynamisk-done

--- js/productlist.js
+++ js/productlist.js
@@ -1,4 +1,7 @@
-const listURL = "https://kea-alt-del.dk/t7/api/products";
+const params = new URLSearchParams(window.location.search);
+const category = params.get("category");
+
+const listURL = "https://kea-alt-del.dk/t7/api/products?category=" + category;
 const listContainer = document.querySelector(".product_gallery_2");
 
 function getProducts() {
@@ -18,9 +21,9 @@ function showProducts(products) {
                     <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="product img">
                     <h3>${product.productdisplayname}</h3>
                     <p>Brandname: ${product.brandname}</p>
-                    <p class="price">Price: $${product.price}</p>
+                    <p class="price">Sub-category: ${product.subcategory}</p>
                      <p class="discountpricer">Price: $${tilbudspris}</p>
-                    <button class="product_preview_btn"><a href="product.html">View product</a></button>
+                    <button class="product_preview_btn"><a href="product.html?id=${product.id}">View product</a></button>
                 </article>
     `;
   });
